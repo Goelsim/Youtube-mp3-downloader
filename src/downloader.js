7cc0ad729c604df7d9f998cab10352e0236dea04
Goelsim <simran.goel1998@gmail.com>
2020-08-12 00:40:45 +0530

    Updated stuff

--- src/downloader.js
+++ src/downloader.js
@@ -1,5 +1,5 @@
 const YouTubeMp3Downloader = require('youtube-mp3-downloader');
-const { isURL } = require("./helpers.js");
+const { isURL } = require("./helpers");
 let url = require('url');
 
 const downloader = new YouTubeMp3Downloader({
@@ -13,17 +13,21 @@ const downloader = new YouTubeMp3Downloader({
 module.exports = {
     download: function(videoIdOrLink, fileName) {
         return new Promise(((resolve, reject) => {
-            if(!videoIdOrLink) {
-                throw new Error("Please enter a valid id or link")
-            }
             let videoId = videoIdOrLink;
             if(isURL(videoIdOrLink)) {
                 let urlQueryObj = url.parse(videoIdOrLink, true).query
                 videoId = urlQueryObj.v;
             }
+
+            if(!videoId) {
+                throw new Error("Please enter a valid id or link")
+            }
             downloader.download(videoId, fileName);
             downloader.on('finished', function(err, data) {
                 resolve(data);
+            });
+            downloader.on('error', function (err) {
+                reject(err);
             });
         }))
     },
---
+++ src/index.js
@@ -0,0 +1,32 @@
+const program = require("commander");
+const Downloader = require('./downloader');
+const ora = require('ora');
+const progress = require('progress');
+
+const spinner = ora('Downloading...').start();
+spinner.color = 'yellow';
+spinner.text = 'Downloading...';
+
+program.version('0.0.1').description("A simple node-cli youtube mp3 downloader");
+
+program.command('ytd')
+    .requiredOption('-l, --link <link>', 'A youtube video link or id')
+    .option('-n, --names [names]', 'Name of the downloaded file')
+    .action((cmObj) => {
+        let {link, name} = cmObj;
+        Downloader.download(link, name)
+            .then(finishedObj => {
+                //console.log("Succeeded.")
+                spinner.succeed(`Finished downloading..."${finishedObj.videoTitle}" in ${finishedObj.stats.runtime} seconds`);
+            }).catch(err => {
+                //console.log("Failed.")
+                spinner.fail("Sorry! Could not download that file. Error occurred.")
+                console.log(err);
+        });
+
+        Downloader.downloader.on('progress', function (progressObj) {
+            spinner.text = `${Number(progressObj.progress.percentage).toFixed(2)}% done`;
+        })
+    });
+
+program.parse(process.argv);
