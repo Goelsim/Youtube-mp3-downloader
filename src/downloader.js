281ddda32747c529a3e445a4b9e6b74e2c5cdb8b
Goelsim <simran.goel1998@gmail.com>
2020-08-11 23:37:08 +0530

    Updated stuff

--- src/downloader.js
+++ src/downloader.js
@@ -1,4 +1,7 @@
 const YouTubeMp3Downloader = require('youtube-mp3-downloader');
+const { isURL } = require("./helpers.js");
+let url = require('url');
+
 const downloader = new YouTubeMp3Downloader({
     ffmpegPath: "/usr/bin/ffmpeg",
     outputPath: "/home/simran/Music",
@@ -6,3 +9,23 @@ const downloader = new YouTubeMp3Downloader({
     queueParallelism: 3,
     progressTimeout: 1000
 });
+
+module.exports = {
+    download: function(videoIdOrLink, fileName) {
+        return new Promise(((resolve, reject) => {
+            if(!videoIdOrLink) {
+                throw new Error("Please enter a valid id or link")
+            }
+            let videoId = videoIdOrLink;
+            if(isURL(videoIdOrLink)) {
+                let urlQueryObj = url.parse(videoIdOrLink, true).query
+                videoId = urlQueryObj.v;
+            }
+            downloader.download(videoId, fileName);
+            downloader.on('finished', function(err, data) {
+                resolve(data);
+            });
+        }))
+    },
+    downloader
+};
